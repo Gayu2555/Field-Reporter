ba0b13c64ea2d9ec15b62b9e4b34bdb32541adef
Gayu Yunma Ramadhan <gayuyunma1123@gmail.com>
2025-01-17 16:09:21 +0700

    Includind depedensi Tailwind dan Jquery

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login System</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="./dist/styles.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="./node_modules/jquery/dist/jquery.min.js"></script>
     <style>
         .error-message {
             @apply mt-2 text-red-600 text-sm;
