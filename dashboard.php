9691050b7e1e27dd9289476d3fbec20200bd2dfe
Gayu Yunma Ramadhan <gayuyunma1123@gmail.com>
2025-01-20 22:25:45 +0700

    test-test vailidasi logout

--- dashboard.php
+++ dashboard.php
@@ -163,7 +163,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
                 </button>
             </div>
             <div class="mt-6 text-center">
-                <button onclick="window.location.href='backend/logout.php'"
+                <button onclick="window.location.href='logout.php'"
                     class="inline-flex justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
                     Logout
                 </button>
--- index.php
+++ index.php
@@ -169,7 +169,7 @@
                         </div>
                     </div>
                     <div class="mt-6 text-center">
-                        <button onclick="window.location.href='backend/logout.php'"
+                        <button onclick="window.location.href='logout.php'"
                             class="inline-flex justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
                             Logout
                         </button>
